3c5645d294aa30ede784bb1035f082745cbfb1f5
coyove <coyove@hotmail.com>
2019-06-13 21:21:59 +0800

    Improve debugprint

--- waitobject/wait.go
+++ waitobject/wait.go
@@ -1,7 +1,7 @@
 package waitobject
 
 import (
-	"log"
+	"fmt"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -43,10 +43,7 @@ func init() {
 			ts.Lock()
 			for i := len(ts.list) - 1; i >= 0; i-- {
 				n := ts.list[i]
-
-				if debug {
-					log.Println("[debug]", repeat, "notifier:", n, now, n.deadline > now)
-				}
+				debugprint("repeated: ", repeat, ", notifier: ", n, ", now: ", now, ", timedout: ", n.deadline > now)
 
 				if n.deadline > now && n.isvalid() {
 					continue
@@ -55,10 +52,8 @@ func init() {
 				// Remove the notifier, and if it is valid, tell its object to time out
 				ts.list = append(ts.list[:i], ts.list[i+1:]...)
 				if n.isvalid() {
+					debugprint("broadcast by wheel")
 					n.obj.mu.Lock()
-					if debug {
-						log.Println("[debug] broadcast by wheel")
-					}
 					n.obj.sig.Broadcast()
 					n.obj.mu.Unlock()
 				}
@@ -75,8 +70,12 @@ func init() {
 			}
 		}
 	}()
+}
 
-	log.SetFlags(log.Lshortfile | log.Lmicroseconds | log.Ltime)
+func debugprint(v ...interface{}) {
+	if debug {
+		fmt.Println(time.Now().Format(time.RFC3339), fmt.Sprint(v...))
+	}
 }
 
 type Object struct {
@@ -94,14 +93,13 @@ func New() *Object {
 
 func (o *Object) Touch(v interface{}) {
 	o.mu.Lock()
-	if debug {
-		log.Println("[debug] broadcast by touching")
-	}
+	debugprint("broadcast by touching")
 	o.v = v
 	o.sig.Broadcast()
 	o.mu.Unlock()
 }
 
+// SetWaitDeadline sets the deadline of Wait(), note that its precision is 1s
 func (o *Object) SetWaitDeadline(t time.Time) {
 	o.mu.Lock()
 	defer o.mu.Unlock()
@@ -119,9 +117,7 @@ func (o *Object) SetWaitDeadline(t time.Time) {
 
 	o.rev = &notifier{deadline: t.Unix(), obj: o}
 	if o.isTimedout() {
-		if debug {
-			log.Println("direct (already) timeout")
-		}
+		debugprint("direct (already) timeout")
 		o.sig.Broadcast()
 		return
 	}
@@ -137,15 +133,11 @@ func (o *Object) isTimedout() bool {
 		return false
 	}
 
-	ns := time.Now().Unix()
-	cd := o.rev.deadline
+	now := time.Now().Unix()
+	out := now >= o.rev.deadline
 
-	a := ns >= cd
-
-	if debug {
-		log.Println("timed out, now:", ns, "deadline:", cd)
-	}
-	return a
+	debugprint("isTimedout: ", out, ", now: ", now, ", deadline: ", o.rev.deadline)
+	return out
 }
 
 func (o *Object) Wait() (interface{}, bool) {
@@ -157,15 +149,11 @@ func (o *Object) Wait() (interface{}, bool) {
 		return nil, false
 	}
 
-	if !debug {
-		o.sig.Wait()
-	} else {
-		log.Println("wait start", o.v)
-		o.sig.Wait()
-		log.Println("wait end", o.v)
-	}
+	debugprint("wait start, v: ", o.v)
+	o.sig.Wait()
+	debugprint("wait end, v: ", o.v)
 
-	// After receiving any data, return early if received data is a timeout event
+	// After receiving any data, return early if it is already timed out
 	if o.isTimedout() {
 		return nil, false
 	}
