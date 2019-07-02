829f060aba7fb38948bc74e8db987b1044fcad08
coyove <coyove@hotmail.com>
2019-07-02 19:58:52 +0800

    WIP

---
+++ writer/w_test.go
@@ -0,0 +1,94 @@
+package writer
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"testing"
+	"time"
+)
+
+func init() {
+	testMode = "test"
+}
+
+func TestWrite(t *testing.T) {
+	w, err := New("/tmp/zzz")
+	if err != nil {
+		panic(err)
+	}
+	for i := 0; i < 1e6; i++ {
+		j := make([]byte, 8)
+		binary.BigEndian.PutUint64(j, uint64(i))
+		w.Write(j)
+	}
+
+	time.Sleep(time.Second * 2)
+
+	buf, _ := ioutil.ReadFile("/tmp/zzz")
+	test := map[uint64]bool{}
+	for i := 0; i < len(buf); i += 8 {
+		j := binary.BigEndian.Uint64(buf[i:])
+		test[j] = true
+	}
+
+	fmt.Println(len(test))
+
+	fmt.Println("exit")
+	os.Remove("/tmp/zzz")
+}
+
+func TestWrite2(t *testing.T) {
+	w, err := New("/tmp/zzz")
+	if err != nil {
+		panic(err)
+	}
+	for i := 0; i < 1e6; i++ {
+		j := make([]byte, 8)
+		binary.BigEndian.PutUint64(j, uint64(i))
+		w.Write(j)
+	}
+}
+
+func foo(a ...interface{}) string {
+	return fmt.Sprint(a...)
+}
+
+func BenchmarkUnixSocket(b *testing.B) {
+	w, _ := New("/tmp/zzz")
+	for i := 0; i < b.N; i++ {
+		w.Write([]byte(foo(i)))
+	}
+
+	os.Remove("/tmp/zzz")
+}
+
+func BenchmarkUnixSocketClass(b *testing.B) {
+	w, _ := New("/tmp/zzz")
+	w.Classifier = func([]byte) bool {
+		return false
+	}
+
+	for i := 0; i < b.N; i++ {
+		w.Write([]byte(foo(i)))
+	}
+
+	os.Remove("/tmp/zzz")
+}
+
+func BenchmarkMemory(b *testing.B) {
+	p := bytes.Buffer{}
+	of, _ := os.Create("/tmp/zzz")
+	for i := 0; i < b.N; i++ {
+		p.Write([]byte(foo(i)))
+		if p.Len() > 1024*1024 {
+			of.Write(p.Bytes())
+			p.Reset()
+		}
+	}
+	of.Write(p.Bytes())
+	of.Close()
+	os.Remove("/tmp/zzz")
+}
